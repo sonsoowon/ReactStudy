66b6df5bdc4d7ff2e49668fc84a0f1b643cf7652
sonsoowon <yuwoo1018@gmail.com>
2022-09-13 12:53:15 +0900

    index.js: root.render의 의미

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ root.render(
   </React.StrictMode>
 );
 
+// 브라우저의 실제 DOM 내부에 리액트 컴포넌트를 렌더링한다
+// public/index.html 에 존재하는 <div id="root"></div>에 렌더링한다
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
